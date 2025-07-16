8dd0fec95249bfe3c86fde8c3072fce1ec8221de
professorbond <profbondexe@gmail.com>
2025-07-16 17:24:42 +0500

    added components to my db and did migrations

--- books/Data/EF/AppDbContext.cs
+++ books/Data/EF/AppDbContext.cs
@@ -12,5 +12,108 @@ namespace MyBlazorApp.Data
         public DbSet<City> Cities { get; set; } = null!;
         public DbSet<Person> People { get; set; } = null!;
         public DbSet<Phone> Phones { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<City>().HasData(
+                new City { Id = 1, Name = "Караганда" },
+                new City { Id = 2, Name = "Астана" },
+                new City { Id = 3, Name = "Алмата" },
+                new City { Id = 4, Name = "Темиртау" },
+                new City { Id = 5, Name = "Шымкент" }
+            );
+
+            builder.Entity<Person>().HasData(
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "Иван",
+                    MiddleName = "Иванович",
+                    LastName = "Иванов",
+                    Note = "Сидел 2 года",
+                    CityId = 1,
+                },
+                new Person
+                {
+                    Id = 2,
+                    FirstName = "Петр",
+                    MiddleName = "Петрович",
+                    LastName = "Петров",
+                    Note = "ну просто Петя",
+                    CityId = 2,
+                },
+                new Person
+                {
+                    Id = 3,
+                    FirstName = "Сергей",
+                    MiddleName = "Сергеевич",
+                    LastName = "Сергеев",
+                    Note = "четкий пацан",
+                    CityId = 3,
+                },
+                new Person
+                {
+                    Id = 4,
+                    FirstName = "Дмитрий",
+                    MiddleName = "Дмитриевич",
+                    LastName = "Дмитриев",
+                    Note = "спортсмен",
+                    CityId = 4,
+                },
+                new Person
+                {
+                    Id = 5,
+                    FirstName = "Андрей",
+                    MiddleName = "Андреевич",
+                    LastName = "Андреев",
+                    Note = "в законе",
+                    CityId = 5,
+                }
+            );
+            builder.Entity<Phone>().HasData(
+                new Phone
+                {
+                    Id = 1,
+                    PhoneNumber = "+77978972679",
+                    IsBankLinked = true,
+                    PhoneType = "сотовый",
+                    PersonId = 1,
+                },
+                new Phone
+                {
+                    Id = 2,
+                    PhoneNumber = "+77777777777",
+                    IsBankLinked = false,
+                    PhoneType = "сотовый",
+                    PersonId = 2,
+                },
+                new Phone
+                {
+                    Id = 3,
+                    PhoneNumber = "+77765672456",
+                    IsBankLinked = true,
+                    PhoneType = "сотовый",
+                    PersonId = 3,
+                },
+                new Phone
+                {
+                    Id = 4,
+                    PhoneNumber = "+77778976655",
+                    IsBankLinked = false,
+                    PhoneType = "сотовый",
+                    PersonId = 4,
+                },
+                new Phone
+                {
+                    Id = 5,
+                    PhoneNumber = "+77770950095",
+                    IsBankLinked = true,
+                    PhoneType = "сотовый",
+                    PersonId = 5,
+                }
+            );
+        }
     }
 }
---
+++ books/Migrations/20250716121525_SeedInitialData.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace books.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "City",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Караганда" },
+                    { 2, "Астана" },
+                    { 3, "Алмата" },
+                    { 4, "Темиртау" },
+                    { 5, "Шымкент" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Person",
+                columns: new[] { "Id", "CityId", "FirstName", "LastName", "MiddleName", "Note" },
+                values: new object[,]
+                {
+                    { 1, 1, "Иван", "Иванов", "Иванович", "Сидел 2 года" },
+                    { 2, 2, "Петр", "Петров", "Петрович", "ну просто Петя" },
+                    { 3, 3, "Сергей", "Сергеев", "Сергеевич", "четкий пацан" },
+                    { 4, 4, "Дмитрий", "Дмитриев", "Дмитриевич", "спортсмен" },
+                    { 5, 5, "Андрей", "Андреев", "Андреевич", "в законе" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Phones",
+                columns: new[] { "Id", "IsBankLinked", "PersonId", "PhoneNumber", "PhoneType" },
+                values: new object[,]
+                {
+                    { 1, true, 1, "+77978972679", "сотовый" },
+                    { 2, false, 2, "+77777777777", "сотовый" },
+                    { 3, true, 3, "+77765672456", "сотовый" },
+                    { 4, false, 4, "+77778976655", "сотовый" },
+                    { 5, true, 5, "+77770950095", "сотовый" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
