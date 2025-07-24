75aae9378d67c9b50eb6df1b0915c868ff950713
professorbond <profbondexe@gmail.com>
2025-07-24 15:04:30 +0500

    #23 добавил двум контактам еще по телефону

--- books/Data/EF/AppDbContext.cs
+++ books/Data/EF/AppDbContext.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using MyBlazorApp.Data.Entities;
+
 namespace MyBlazorApp.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
+
         public DbSet<City> Cities { get; set; } = null!;
         public DbSet<Person> People { get; set; } = null!;
         public DbSet<Phone> Phones { get; set; } = null!;
@@ -72,6 +73,7 @@ namespace MyBlazorApp.Data
                     CityId = 5,
                 }
             );
+
             builder.Entity<Phone>().HasData(
                 new Phone
                 {
@@ -112,6 +114,22 @@ namespace MyBlazorApp.Data
                     IsBankLinked = true,
                     PhoneType = "сотовый",
                     PersonId = 5,
+                },
+                new Phone
+                {
+                    Id = 6,
+                    PhoneNumber = "+77001112233",
+                    IsBankLinked = false,
+                    PhoneType = "домашний",
+                    PersonId = 1,
+                },
+                new Phone
+                {
+                    Id = 7,
+                    PhoneNumber = "+77005556677",
+                    IsBankLinked = true,
+                    PhoneType = "рабочий",
+                    PersonId = 2,
                 }
             );
         }
---
+++ books/Migrations/20250724095434_AddMorePhones.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace books.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMorePhones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Phones",
+                columns: new[] { "Id", "IsBankLinked", "PersonId", "PhoneNumber", "PhoneType" },
+                values: new object[,]
+                {
+                    { 6, false, 1, "+77001112233", "домашний" },
+                    { 7, true, 2, "+77005556677", "рабочий" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
